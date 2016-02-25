aafac41bf41bc41cb7dbb7fecd24a62a157d0b1e
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-02-25 16:13:19 +0100

    Stylistic changes in current streak (streak-item).

--- app/components/streak-item.js
+++ app/components/streak-item.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import config from 'travis/config/environment';
 
 export default Ember.Component.extend({
+  classNames: ['streak-item'],
 
   getStreakData: function() {
     var result, apiEndpoint, options, repoId;
