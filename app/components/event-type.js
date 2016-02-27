2443bf474f12db0f91d88a90a37ed35d4a2a77d3
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-02-27 17:25:43 +0100

    add basic chart drawing, to be styled

--- app/components/event-type.js
+++ app/components/event-type.js
@@ -27,6 +27,10 @@ export default Ember.Component.extend({
     return eventDict;
   },
 
+  cleanUp: function() {
+    // TODO
+  }.property("repo", "isLoading"),
+
   load: function() {
     this.set("isLoading", true);
     var self = this;
@@ -40,7 +44,7 @@ export default Ember.Component.extend({
       };
     }
 
-    $.ajax(apiEndpoint + "/v3/repo/" + repoId + "/overview/event_type", options)
+    $.ajax(apiEndpoint + "/v3/repo/" + repoId + "/overview/event_type_data", options)
     .then(function(response) {
       self.set("data", self.convertData(response));
       self.set("isLoading", false);
@@ -48,4 +52,99 @@ export default Ember.Component.extend({
     return "";
   }.property("repo"),
 
+  draw: function() {
+    var data = this.get("data");
+    console.log(data);
+    var events = Object.keys(data);
+    console.log(events);
+    for(var i=0; i<events.length; i++) {
+      drawOnePie(events[i], data[events[i]]);
+    }
+
+    function drawOnePie(eventType, data) {
+      var width = 400,
+      height = 200,
+      radius = Math.min(width, height) / 2;
+
+      var arc = d3.svg.arc()
+      .outerRadius(radius-10)
+      .innerRadius(0);
+
+      var labelArc = d3.svg.arc()
+      .outerRadius(radius - 40)
+      .innerRadius(radius - 40);
+
+      var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return d.count; });
+
+      var svg = d3.select(".event-type")
+      .append("div")
+      .attr("id", "build_history_chart")
+      .classed("svg-container", true)
+      .append("svg")
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 " + width + " " + height)
+      .classed("svg-content-responsive", true)
+      .append("g")
+      .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+
+      var g = svg.selectAll(".arc")
+      .data(pie(data))
+      .enter().append("g")
+      .attr("class", "arc");
+
+      var piePieceMouseEnter = function() {
+        var text = d3.select(this).attr("hovertext");
+        var x = d3.mouse(this)[0];
+        var y = d3.mouse(this)[1];
+
+        var labelGroup = svg.append("g")
+        .attr("class", "bar-label");
+
+        var labelRect = labelGroup.append("rect")
+        .attr("x", 0)
+        .attr("y", 0);
+
+        var labelText = labelGroup.append("text")
+        .attr("x", 10)
+        .attr("y", 5)
+        .attr("dy", "1em")
+        .text(text);
+
+        labelRect
+        .attr("height", labelText.node().getBBox().height + 10)
+        .attr("width", labelText.node().getBBox().width + 20);
+
+        var offsetX = x - labelGroup.node().getBBox().width - 5;
+        var offsetY = y - labelGroup.node().getBBox().height - 5;
+        labelGroup.attr("transform", "translate(" + offsetX + "," + offsetY + ")");
+      };
+
+      var piePieceMouseLeave = function() {
+        d3.selectAll(".bar-label").remove();
+      };
+
+      var piePieceMouseMove = function() {
+        var labelGroup = d3.select(".bar-label");
+        var x = d3.mouse(this)[0];
+        var y = d3.mouse(this)[1];
+
+        var offsetX = x - labelGroup.node().getBBox().width - 5;
+        var offsetY = y - labelGroup.node().getBBox().height - 5;
+        labelGroup.attr("transform", "translate(" + offsetX + "," + offsetY + ")");
+      };
+
+      var piePieces = g.append("path")
+      .attr("d", arc)
+      .attr("class", function(d) { return d.data.state + " pie-piece"; })
+      .attr("hovertext", function(d) { return d.data.state; });
+
+      piePieces
+      .on("mouseenter", piePieceMouseEnter)
+      .on("mouseleave", piePieceMouseLeave)
+      .on("mousemove" , piePieceMouseMove );
+
+    }
+  }.property("repo", "isLoading")
 });
