1fde843106089d4302aebc1a4f49bda055ed4695
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-03-01 09:25:59 +0100

    fix linter warning

--- app/components/branch_table.js
+++ app/components/branch_table.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   classNames: ['branch_table'],
 
   repo_branches: function() {
-    return ['master', 'pharo']
+    return ['master', 'pharo'];
   }.property('repo'),
 
   getBranchData: function() {
@@ -22,8 +22,11 @@ export default Ember.Component.extend({
     $.ajax(apiEndpoint + "/v3/repo/" + repoId + "/overview/branches", options).then(function(response) {
       var array = [];
       for(var branchname in response.branches){
-        array.push(Ember.Object.create({branchname: branchname, percentage: Math.round(100*response.branches[branchname])}));
-      };
+        array.push(Ember.Object.create({
+          branchname: branchname,
+          percentage: Math.round(100*response.branches[branchname])
+        }));
+      }
       array.sort(function(a, b){
         return a.branchname>b.branchname;
       });
